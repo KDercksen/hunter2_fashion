9bac32b0f04fb3d2002e9e740e4c9348df6e20f6
KDercksen <mail@koendercksen.com>
2018-06-08 21:25:45 +0200

    things

--- fashion_code/util.py
+++ fashion_code/util.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from keras.preprocessing.image import img_to_array, load_img
-from tensorflow.python.lib.io import file_io
-import os
-import io
 from PIL import Image
+from fashion_code.constants import paths
+from keras.preprocessing.image import img_to_array, load_img
+from sklearn.preprocessing import MultiLabelBinarizer
+import io
 import numpy as np
+import os
+import pandas as pd
 
 
 def read_img(fname, size, gcp=False):
@@ -19,3 +21,25 @@ def read_img(fname, size, gcp=False):
     else:
         img = load_img(fname, target_size=size)
         return img_to_array(img)
+
+
+def create_submission(y_pred, filename):
+        preds = y_pred > .5
+        classes = pd.read_csv(paths['dummy']['csv'])
+
+        print('Converting labels...')
+        mlb = MultiLabelBinarizer(classes=classes)
+        mlb.fit(None) # necessary, won't actually do anything
+        sparse_preds = mlb.inverse_transform(preds)
+
+        submission_list = []
+        for i, p in enumerate(sparse_preds, start=1):
+            labels = ' '.join(p)
+            submission_list.append([i, labels])
+
+        submission_path = join(paths['results'],
+                               '{}-submission.csv'.format(filename))
+        print('Saving predictions to {}'.format(submission_path))
+        columns = ['image_id', 'label_id']
+        pd.DataFrame(submission_list, columns=columns) \
+                    .to_csv(submission_path, index=False)
--- scripts/prediction_transformer.py
+++ scripts/prediction_transformer.py
@@ -2,34 +2,61 @@
 # -*- coding: utf-8 -*-
 
 from argparse import ArgumentParser
-from fashion_code.callbacks import F1Utility
 from fashion_code.constants import num_classes, paths
 from fashion_code.generators import SequenceFromDisk
+from fashion_code.util import create_submission
 from keras.applications.xception import preprocess_input
-from keras.layers import Dense, Input
+from keras.layers import Dense, Dropout, Input
 from keras.models import Model, load_model
 from keras.optimizers import Adam
+from keras.utils import multi_gpu_model
 from os.path import join
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 import numpy as np
 import sys
+import tensorflow as tf
 
 
-def nn_transformer(model):
-    x = model.output
+def rf_transformer():
+    return RandomForestClassifier(n_estimators=200, n_jobs=8, verbose=1)
+
+
+def nn_transformer():
+    inputs = Input(shape=(num_classes,))
+    x = inputs
     for i in range(4):
         x = Dense(num_classes*(2+i), activation='relu',
                   name='trans_dense_{}'.format(i),
                   kernel_initializer='he_normal')(x)
+        x = Dropout(.3, name='trans_dropout_{}'.format(i))(x)
     outputs = Dense(num_classes, activation='sigmoid', name='trans_out',
                     kernel_initializer='he_normal')(x)
-    transformer = Model(model.inputs, outputs)
-
-    for layer in model.layers:
-        layer.trainable = False
+    transformer = Model(inputs, outputs)
 
     optimizer = Adam(decay=1e-5)
     transformer.compile(optimizer=optimizer, loss='binary_crossentropy')
     return transformer
+
+
+def generate_data(model, batch_size):
+    train_gen = SequenceFromDisk('train', batch_size, (299, 299),
+                                 preprocessfunc=preprocess_input)
+    valid_gen = SequenceFromDisk('validation', batch_size, (299, 299),
+                                 preprocessfunc=preprocess_input)
+
+    x_train = model.predict_generator(train_gen,
+                                      use_multiprocessing=True,
+                                      workers=8,
+                                      verbose=1)
+    x_valid = model.predict_generator(valid_gen,
+                                      use_multiprocessing=True,
+                                      workers=8,
+                                      verbose=1)
+    y_train = np.load(join(paths['data'], 'labels_train.npy'))
+    y_valid = np.load(join(paths['data'], 'labels_validation.npy'))
+
+    return x_train, x_valid, y_train, y_valid
 
 
 if __name__ == '__main__':
@@ -40,39 +67,44 @@ if __name__ == '__main__':
                     help='Model to train transformer on top of')
     p.add_argument('--epochs', type=int, default=10, help='Epochs')
     p.add_argument('--batch-size', type=int, default=128, help='Batch size')
-    p.add_argument('--train-steps', type=int, help='Steps')
     p.add_argument('--create-submission', action='store_true')
     args = p.parse_args()
 
     batch_size = args.batch_size
     fname = join(paths['models'], args.filename)
-    model = load_model(fname)
+    save_filename = args.save_filename
+    model = multi_gpu_model(load_model(fname), cpu_relocation=True)
     threshold = .5
     epochs = args.epochs
 
-    train_gen = SequenceFromDisk('train', batch_size, (299, 299),
-                                 preprocessfunc=preprocess_input)
-    valid_gen = SequenceFromDisk('validation', batch_size, (299, 299),
-                                 preprocessfunc=preprocess_input)
+    x_train, x_valid, y_train, y_valid = generate_data(model, batch_size)
+
+    # TODO: add random forest classifier
+
+    transformer = nn_transformer()
+    transformer.fit(x_train, y_train,
+                    validation_data=(x_valid, y_valid),
+                    epochs=epochs,
+                    verbose=1)
+    transformer.save(join(paths['models'], save_filename))
+
+    valid_preds = transformer.predict(x_valid,
+                                      batch_size=batch_size,
+                                      verbose=1) > threshold
+    score = f1_score(y_valid, valid_preds, average='micro')
+    print('F1 score on validation: {:.4f}'.format(score))
 
     if args.create_submission:
+        print('Creating submission...')
         test_gen = SequenceFromDisk('test', batch_size, (299, 299),
                                     preprocessfunc=preprocess_input)
-    else:
-        test_gen = None
-
-    train_steps = args.train_steps or len(train_gen)
-    transformer = nn_transformer(model)
-
-    pm = F1Utility(valid_gen, test_generator=test_gen,
-                   save_path=paths['models'], save_fname=args.save_filename)
-
-    transformer.fit_generator(train_gen,
-                              epochs=epochs,
-                              steps_per_epoch=train_steps,
-                              use_multiprocessing=True,
-                              workers=8,
-                              callbacks=[pm],
-                              verbose=1)
+        initial_preds = model.predict_generator(test_gen,
+                                                use_multiprocessing=True,
+                                                workers=8,
+                                                verbose=1)
+        y_pred = transformer.predict(initial_preds,
+                                     batch_size=batch_size,
+                                     verbose=1)
+        create_submission(y_pred, save_filename)
 
     sys.exit(0)
